3af54f0faef77e6d2dff50d4bd49c0a566da016b
Miguel Alcaino <miguel.alcaino@ogb.cl>
2017-02-08 01:22:40 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+cocavi
+======
+
+A Symfony project created on February 7, 2017, 10:23 pm.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        $baseUrl = 'http://www.rinoceronte.fm/podcast/9';
+        if($request->query->has('page')){
+            $url = $baseUrl.'?Podcast_page='.$request->query->get('page');
+        }else{
+            $url = $baseUrl;
+        }
+
+        $html = file_get_contents($url);
+
+        $crawler = new Crawler($html);
+        $pagination = $crawler->filter('ul.pagination li');
+
+        $crawler = $crawler->filter('div.items li a');
+
+        $arrayOfPodcasts  = array();
+        /** @var \DOMElement $domElement */
+        foreach ($crawler as $domElement) {
+            $onclick = $domElement->getAttribute('onclick');
+            $link = str_replace('doSet(','',$onclick);
+            $link = str_replace(')','',$link);
+            $data = explode(',',$link);
+            $link = str_replace("'",'',$data[0]);
+            $arrayOfPodcasts[] = array(
+                'link' => $link,
+                'date' => str_replace("'",'',$data[1])
+            );
+
+        }
+
+        $pages = array();
+
+        /** @var \DOMElement $domElement */
+        foreach ($pagination as $domElement){
+            if(is_numeric($domElement->textContent)){
+                $pages[] = $domElement->textContent;
+            }
+        }
+
+        return $this->render('@App/Default/index.html.twig', array(
+            'podcasts' => $arrayOfPodcasts,
+            'pages' => $pages
+        ));
+    }
+}
